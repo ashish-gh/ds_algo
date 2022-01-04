8116750f6318d3c16bfb60be57d39a353438e03b
Ashish Ghimire <ashish.ghimire@docsumo.com>
2022-01-04 08:08:48 +0545

    Permutation

--- leetcode/all_problems/0046_permutations.py
+++ leetcode/all_problems/0046_permutations.py
@@ -1,53 +1,37 @@
-# Given an array return the permutations of the numbers, this can be in any order
-
+# Question:
 
 
 from typing import List
 
 
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        if len(nums) == 1:
-            return [nums[:]]
+    def permute(self, nums: List[int]) -> List[int]:
+        # check base case
+        if len(nums) == 1: return [nums[:]]
         
-        for i in range(len(nums)):
-            n = nums.pop(0)
-            perms = self.permute(nums)
+        result = []
+        for _ in range(len(nums)):
 
+            # pop the first elements of the list and 
+            # for the remaining elements find the permutation by recursive methods
+            n = nums.pop(0)
+            perms = self.permute(nums=nums)
+            # for every new permutatio, we append the popped previous element to the new permutation
             for perm in perms:
                 perm.append(n)
+            # now every permutation is extended to the final results
             result.extend(perms)
+            # append the popped element back to the nums
             nums.append(n)
         return result
-
-
-class Solution1:
-    def __init__(self) -> None:
-        self.res = []
-
-    def permute(self, nums, path):
-        self.backtrack(nums, path)
-        return self.res
-    
-    def backtrack(self, nums, path):
-        if not nums:
-            self.res.append(path)
-        print(f"Nums <<<<<<<<<<< {nums}")
-        for i in range(len(nums)):
-            new_path= path + [nums[i]]
-            print(f"New path : {new_path}")
-            nums_ = nums[:i] + nums[i+1:]
-            print(f"{nums[:i] }  | {nums[i+1:]} | Nums : {nums_}")
-            self.backtrack(nums_, new_path)
-
+        
 
 def main():
-    sl = Solution1()
-    nums = [1,2,3]
-    print(sl.permute(nums, []))
+    sl = Solution()
+    nums = [1,1,3]
+    res  = sl.permute(nums=nums)
+    print(f"List of perms : {res}")
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
-    
